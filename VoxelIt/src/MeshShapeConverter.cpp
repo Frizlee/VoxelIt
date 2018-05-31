b1f454c15fb1a78b8f48f6df653bb482dff52e26
Frizlee <krys.dziewa@gmail.com>
2018-05-31 20:04:20 +0200

    generate voxel shape

--- VoxelIt/src/MeshShapeConverter.cpp
+++ VoxelIt/src/MeshShapeConverter.cpp
@@ -1,4 +1,4 @@
-#include <VoxelIt\MeshShapeConverter.hpp>
+#include <VoxelIt/MeshShapeConverter.hpp>
 #include <fstream>
 using namespace vit;
 using namespace std;
@@ -16,6 +16,7 @@ MeshShapeConverter::MeshShapeConverter(float voxelSize, bool overrideSize)
 
 shared_ptr<Shape> VOXELIT_API MeshShapeConverter::getShapeFromMesh(shared_ptr<const Mesh> mesh)
 {
+    vector<Voxel> voxels;
     auto bBox = mesh->getBoundingBox();
     auto faces = mesh->getFaces();
     auto vertices = mesh->getVertices();
@@ -29,6 +30,11 @@ shared_ptr<Shape> VOXELIT_API MeshShapeConverter::getShapeFromMesh(shared_ptr<co
     vector<uint8_t> xyPlane(width * height);
     vector<uint8_t> zyPlane(depth * height);
     vector<uint8_t> xzPlane(width * depth);
+
+    int32_t xyCount = 0;
+    int32_t zyCount = 0;
+    int32_t xzCount = 0;
+    int i = 0;
 
     for (auto& face : faces)
     {
@@ -51,7 +57,10 @@ shared_ptr<Shape> VOXELIT_API MeshShapeConverter::getShapeFromMesh(shared_ptr<co
                 auto rectOrig = vec2(i * mVoxelSize, j * mVoxelSize);
 
                 if (checkIntersect(rectOrig, mVoxelSize, vec2(a.x, a.y), vec2(b.x, b.y), vec2(c.x, c.y)))
+                {
                     xyPlane[(j - planesLower.y) * width + (i - planesLower.x)] = 1;
+                    ++xyCount;
+                }
             }
         }
 
@@ -66,7 +75,10 @@ shared_ptr<Shape> VOXELIT_API MeshShapeConverter::getShapeFromMesh(shared_ptr<co
                 auto rectOrig = vec2(i * mVoxelSize, j * mVoxelSize);
 
                 if (checkIntersect(rectOrig, mVoxelSize, vec2(a.z, a.y), vec2(b.z, b.y), vec2(c.z, c.y)))
+                {
                     zyPlane[(j - planesLower.y) * depth + (i - planesLower.z)] = 1;
+                    ++zyCount;
+                }
             }
         }
 
@@ -81,21 +93,77 @@ shared_ptr<Shape> VOXELIT_API MeshShapeConverter::getShapeFromMesh(shared_ptr<co
                 auto rectOrig = vec2(i * mVoxelSize, j * mVoxelSize);
 
                 if (checkIntersect(rectOrig, mVoxelSize, vec2(a.x, a.z), vec2(b.x, b.z), vec2(c.x, c.z)))
+                {
                     xzPlane[(j - planesLower.z) * width + (i - planesLower.x)] = 1;
+                    ++xzCount;
+                }
+            }
+        }
+
+
+        // Create voxels.
+        if (xyCount >= zyCount && xyCount >= xzCount)
+        {
+            for (int32_t j = xyBBox.y; j < xyBBox.w; ++j)
+            {
+                for (int32_t i = xyBBox.x; i < xyBBox.z; ++i)
+                {
+                    if (xyPlane[(j - planesLower.y) * width + (i - planesLower.x)] != 1)
+                        continue;
+
+                    int32_t z = planeFunc(a, b, c, i * mVoxelSize + mVoxelSize / 2, j * mVoxelSize + mVoxelSize / 2) / mVoxelSize;
+                    voxels.push_back(Voxel({ i, j, z }));
+                }
+            }
+        }
+        else if (zyCount >= xyCount && zyCount >= xzCount)
+        {
+            for (int32_t j = zyBBox.y; j < zyBBox.w; ++j)
+            {
+                for (int32_t i = zyBBox.x; i < zyBBox.z; ++i)
+                {
+                    if (zyPlane[(j - planesLower.y) * depth + (i - planesLower.z)] != 1)
+                        continue;
+
+                    auto newA = vec3(a.z, a.y, a.x);
+                    auto newB = vec3(b.z, b.y, b.x);
+                    auto newC = vec3(c.z, c.y, c.x);
+                    int32_t x = planeFunc(newA, newB, newC, i * mVoxelSize + mVoxelSize / 2, j * mVoxelSize + mVoxelSize / 2) / mVoxelSize;
+                    voxels.push_back(Voxel({ x, j, i }));
+                }
+            }
+        }
+        else if (xzCount >= xyCount && xzCount >= zyCount)
+        {
+            for (int32_t j = xzBBox.y; j < xzBBox.w; ++j)
+            {
+                for (int32_t i = xzBBox.x; i < xzBBox.z; ++i)
+                {
+                    if (xzPlane[(j - planesLower.z) * width + (i - planesLower.x)] != 1)
+                        continue;
+
+                    auto newA = vec3(a.x, a.z, a.y);
+                    auto newB = vec3(b.x, b.z, b.y);
+                    auto newC = vec3(c.x, c.z, c.y);
+                    int32_t y = planeFunc(newA, newB, newC, i * mVoxelSize + mVoxelSize / 2, j * mVoxelSize + mVoxelSize / 2) / mVoxelSize;
+                    voxels.push_back(Voxel({ i, y, j }));
+                }
             }
         }
     }
 
-    ofstream ofile1("xy.raw", ios_base::binary);
-    for (auto c : xyPlane)
+    for (size_t i = 0; i < voxels.size(); ++i)
     {
-        if (c == 1)
-            ofile1 << (uint8_t)0xff;
-        else
-            ofile1 << (uint8_t)0x00;
+        for (auto it = begin(voxels) + i + 1; it != end(voxels);)
+        {
+            if (voxels[i].getPosition() == it->getPosition())
+                it = voxels.erase(it);
+            else
+                ++it;
+        }
     }
 
-    return make_shared<Shape>();
+    return make_shared<Shape>(mVoxelSize, voxels);
 }
 
 shared_ptr<Mesh> VOXELIT_API MeshShapeConverter::getMeshFromShape(shared_ptr<const Shape> shape)
@@ -189,4 +257,16 @@ bool MeshShapeConverter::checkPointInRectangle(vec2 p, vec2 rectOrig, float widt
     auto v = p - rectOrig;
     return v.x <= width && v.x >= 0 &&
         v.y <= width && v.y >= 0;
+}
+
+float MeshShapeConverter::planeFunc(vec3 p, vec3 q, vec3 r, float x, float y)
+{
+    auto v1 = p - q;
+    auto v2 = p - r;
+    auto cross12 = cross(v1, v2);
+    auto a = cross12.x;
+    auto b = cross12.y;
+    auto c = cross12.z;
+
+    return (a * (x + p.x) + b * (y + p.y)) / -c - p.z;
 }
